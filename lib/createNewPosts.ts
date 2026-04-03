7b38f0cccf46a002dad51d34ebd85848100b4df0
Danaid Sinani <danaid@bu.edu>
2026-04-03 15:02:32 -0400

    dis9

--- lib/createNewPosts.ts
+++ lib/createNewPosts.ts
@@ -1,24 +1,25 @@
 "use server";
 
 import { PostProps } from "@/types";
-import getCollection, {POSTS_COLLECTION} from "@/db";
+import getCollection, { POSTS_COLLECTION } from "@/db";
 
-
-export default async function createNewPost(title:string, content:string,):Promise<PostProps | null>{
-  const p ={
+export default async function createNewPost(
+  title: string,
+  content: string,
+): Promise<PostProps | null> {
+  const p = {
     title: title,
     content: content,
     upvotes: 0,
     downvotes: 0,
-  }
+  };
 
   const postsCollection = await getCollection(POSTS_COLLECTION);
   const res = await postsCollection.insertOne(p);
 
-  if(!res.acknowledged){
+  if (!res.acknowledged) {
     return null;
   }
 
-
-  return {...p, id: res.insertedId.toHexString()};
+  return JSON.parse(JSON.stringify({ ...p, id: res.insertedId.toHexString() })) as PostProps;
 }
