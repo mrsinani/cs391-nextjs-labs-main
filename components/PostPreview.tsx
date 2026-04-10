11d3301c26bf10c6bb34dddbbe674ce9ea9585d2
Danaid Sinani <danaid@bu.edu>
2026-04-10 14:33:46 -0400

    Update PostPreview.tsx

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -5,7 +5,7 @@ export default function PostPreview({post}: { post: PostProps }) {
     return (
         <Link href={`/post/${post.id}`}>
             <div className="bg-sky-400 rounded-xl p-4 m-2 w-96">
-                <h4 className="font-bold text-3xl">{post.title}</h4>
+                <h4 className="font-bold text-3xl ">{post.title}</h4>
                 <p>{String(Number(post.upvotes) - Number(post.downvotes))}</p>
             </div>
         </Link>
